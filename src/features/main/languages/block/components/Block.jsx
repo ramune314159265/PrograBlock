c57509be18081eee7e2d241f681de2dadb06e3e5
ramune314159265 <ramune314159265@gmail.com>
2026-07-01 17:52:44 +0900

    a

--- src/features/main/languages/block/components/Block.jsx
+++ src/features/main/languages/block/components/Block.jsx
@@ -1,87 +1,82 @@
-import { Feedback } from "@dnd-kit/dom";
-import { DragDropProvider } from "@dnd-kit/react";
-import { getFromPath } from "../../../util/getFromPath";
-import { IrContext } from "./Context";
-import { Statements } from "./Statements";
+import { Box } from '@chakra-ui/react';
+import * as Blockly from 'blockly/core';
+import * as Ja from 'blockly/msg/ja';
+import { useEffect, useRef } from 'react';
+
+Blockly.common.defineBlocksWithJsonArray([
+	{
+		"type": "variable_declaration",
+		"tooltip": "変数、定数の定義をします",
+		"helpUrl": "",
+		"message0": "変数 %1 に %2 を代入 %3",
+		"args0": [
+			{
+				"type": "field_input",
+				"name": "NAME",
+				"text": "name"
+			},
+			{
+				"type": "input_value",
+				"name": "init"
+			},
+			{
+				"type": "input_dummy",
+				"name": "NAME"
+			}
+		],
+		"previousStatement": null,
+		"nextStatement": null,
+		"colour": 225
+	}
+]);
+
+const toolbox = {
+	kind: 'flyoutToolbox',
+	contents: [
+		{
+			kind: 'block',
+			type: 'variable_declaration'
+		}
+	]
+}
 
 export const Block = ({ ir, setIr }) => {
+	const containerRef = useRef()
+	const initializedRef = useRef(false)
+	useEffect(() => {
+		if (!containerRef.current || initializedRef.current) {
+			return
+		}
+
+		initializedRef.current = true
+		Blockly.setLocale(Ja)
+		const workspace = Blockly.inject(containerRef.current, {
+			toolbox,
+			move: {
+				scrollbars: {
+					horizontal: true,
+					vertical: true,
+				},
+				drag: true,
+				wheel: false,
+			},
+			renderer: 'Zelos'
+		})
+
+		workspace.addChangeListener(() => {
+			// if (event.isUiEvent) return
+			const json = Blockly.serialization.workspaces.save(workspace)
+			console.log(json)
+		})
+	}, [containerRef])
+
 	return (
-		<DragDropProvider
-			plugins={(defaults) => [
-				...defaults,
-				Feedback.configure({ dropAnimation: null }),
-			]}
-			onDragEnd={(e) => {
-				if (e.canceled) return;
-				const from = e.operation.source?.data?.path;
-				if (!from) {
-					return;
-				}
-				const fromPath = from.slice(0, -1);
-				const fromProperty = from.at(-1);
-				const fromType = e.operation.source?.data?.type;
-
-				const to = e.operation.target?.data?.path;
-				if (!to) {
-					return;
-				}
-				const toPath = to.slice(0, -1);
-				const toProperty = to.at(-1);
-				const toType = e.operation.target?.data?.type;
-				if (
-					e.operation.source?.data?.type === "statement" &&
-					e.operation.target?.data?.type === "expression"
-				) {
-					return;
-				}
-
-				console.log({
-					from,
-					fromPath,
-					fromProperty,
-					to,
-					toPath,
-					toProperty,
-				});
-				const copy = structuredClone(ir);
-				const copyFrom = getFromPath(copy, fromPath);
-				const copyTo = getFromPath(copy, toPath);
-				switch (true) {
-					case fromType === "statement" && toType === "statement": {
-						const fromId = copyFrom[fromProperty].id;
-						copyTo.splice(
-							toProperty + 1,
-							0,
-							copyFrom[fromProperty],
-						);
-						const fromIndex = copyFrom.findIndex(
-							(e) => e.id === fromId,
-						);
-						copyFrom.splice(fromIndex, 1);
-						setIr(copy);
-						break;
-					}
-					case fromType === "statement" && toType === "expression":
-						break;
-					case fromType === "expression" && toType === "statement": {
-					}
-					case fromType === "expression" && toType === "expression": {
-						copyTo[toProperty] = copyFrom[fromProperty];
-						const fromData = structuredClone(
-							copyFrom[fromProperty],
-						);
-						copyFrom[fromProperty] = {
-							type: "empty",
-							placeholder: fromData,
-						};
-						setIr(copy);
-					}
-				}
-			}}
+		<Box
+			ref={containerRef}
+			width="full"
+			height="full"
 		>
-			<IrContext value={[ir, setIr]}>
-				<Statements path={[]}></Statements>
-			</IrContext>
-		</DragDropProvider>
-	);
+
+		</Box>
+	)
 };
