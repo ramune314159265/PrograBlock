3a42fb1077f07924737004fd6b06819efc075957
ramune314159265 <ramune314159265@gmail.com>
2026-07-13 17:56:15 +0900

    実行

--- src/features/main/panels/output/components/Output.jsx
+++ src/features/main/panels/output/components/Output.jsx
@@ -1,62 +1,103 @@
-import { Box } from '@chakra-ui/react'
-import { WebContainer } from '@webcontainer/api'
-import { FitAddon } from '@xterm/addon-fit'
-import { Terminal } from '@xterm/xterm'
-import '@xterm/xterm/css/xterm.css'
-import { useEffect, useRef } from 'react'
+import { Box, HStack, IconButton, SimpleGrid } from "@chakra-ui/react";
+import { WebContainer } from "@webcontainer/api";
+import { FitAddon } from "@xterm/addon-fit";
+import { Terminal } from "@xterm/xterm";
+import { HiPlay } from "react-icons/hi2";
+import "@xterm/xterm/css/xterm.css";
+import { useEffect, useRef } from "react";
+import { useAtom } from "jotai";
+import { processAtom } from "../../../states/runtime";
+import { javascriptContentAtom } from "../../../states/editor";
 
 export const Output = () => {
-	const containerRef = useRef(null)
-	const termRef = useRef()
-	const webContainerRef = useRef(null)
+	const [process, setProcess] = useAtom(processAtom)
+	const [javaScriptContent, setJavaScriptContent] = useAtom(javascriptContentAtom)
+
+	const containerRef = useRef(null);
+	const termRef = useRef();
+	const webContainerRef = useRef(null);
+	const filesRef = useRef(null);
 	useEffect(() => {
 		if (!containerRef.current || termRef.current) {
-			return
+			return;
 		}
 
-		termRef.current = new Terminal()
-		const fitAddon = new FitAddon()
-		termRef.current.loadAddon(fitAddon)
-		termRef.current.open(containerRef.current)
+		termRef.current = new Terminal();
+		const fitAddon = new FitAddon();
+		termRef.current.loadAddon(fitAddon);
+		termRef.current.open(containerRef.current);
 		const resizeObserver = new ResizeObserver(() => {
-			fitAddon.fit()
-		})
-		resizeObserver.observe(containerRef.current)
-	})
+			fitAddon.fit();
+		});
+		resizeObserver.observe(containerRef.current);
+	});
 	useEffect(() => {
 		if (webContainerRef.current || !termRef.current) {
-			return
+			return;
 		}
 		webContainerRef.current = 1;
 
 		(async () => {
-			webContainerRef.current = await WebContainer.boot()
-			const files = {
-				'index.js': {
+			webContainerRef.current = await WebContainer.boot();
+			filesRef.current = {
+				"index.js": {
 					file: {
-						contents: 'console.log("123")'
-					}
-				}
-			}
-			webContainerRef.current.mount(files)
-			const process = await webContainerRef.current.spawn('node', ['index.js'])
+						contents: 'console.log("123")',
+					},
+				},
+			};
+			webContainerRef.current.mount(filesRef.current);
+			const process = await webContainerRef.current.spawn("node", [
+				"index.js",
+			]);
 			process.output.pipeTo(
 				new WritableStream({
 					write(data) {
 						termRef.current.write(data);
 					},
-				})
-			)
-		})()
-	})
+				}),
+			);
+		})();
+	});
+
+	const run = async (content) => {
+		console.log(webContainerRef.current)
+		webContainerRef.current.fs.writeFile('index.js',content)
+		const process = await webContainerRef.current.spawn("node", [
+			"index.js",
+		]);
+		process.output.pipeTo(
+			new WritableStream({
+				write(data) {
+					termRef.current.write(data);
+				},
+			}),
+		);
+	}
 
 	return (
-		<Box
-			ref={containerRef}
-			width='full'
-			height='full'
-			overflow='hidden'
+		<SimpleGrid
+			gridTemplateColumns={"2.5rem calc(100% - 2.5rem)"}
+			width="full"
+			height="full"
+			overflow="hidden"
 		>
-		</Box>
-	)
-}
+			<HStack
+				width="full"
+				height="full"
+				justifyContent="center"
+				alignItems="flex-start"
+			>
+				<IconButton size="sm" variant="ghost" onClick={() => run(javaScriptContent)}>
+					<HiPlay />
+				</IconButton>
+			</HStack>
+			<Box
+				ref={containerRef}
+				width="full"
+				height="full"
+				overflow="hidden"
+			></Box>
+		</SimpleGrid>
+	);
+};
